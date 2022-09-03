fe1044ea010e827dd34e2986690c688fe9396f59
komo1048 <komo1048@naver.com>
2022-09-03 22:37:23 +0900

    영화 api를 불러와 뿌려주기

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,36 +2,43 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  const [money, setMoney] = useState(0);
-  const inputMoney = (event) => {
-    setMoney(event.target.value);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+    );
+
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log(movies);
+
   return (
     <div>
-      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
-      <div>
-        <input type="number" onChange={inputMoney} />
-      </div>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select>
-          {coins.map((item) => (
-            <option key={item.id}>
-              {item.name} ({item.symbol}) : {item.quotes["USD"].price} you can
-              buy coin : {money / item.quotes["USD"].price}
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
     </div>
   );
